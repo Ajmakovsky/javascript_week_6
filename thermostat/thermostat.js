ecb9faf4a958cee59c1c6b012dda35b3eca4ef7b
Abigail <ajmakovsky@gmail.com>
2023-02-22 16:51:31 +0000

    Thermostat Challenge completed and all tests passing

--- thermostat/thermostat.js
+++ thermostat/thermostat.js
@@ -1,11 +1,48 @@
 class Thermostat {
   constructor(){
     this.temperature = 20
+    this.maxtemp = 25
   }
 
   getTemperature = () => {
     return this.temperature
   }
+
+  upTemp = () => {
+    if(this.temperature < this.maxtemp){
+      this.temperature += 1
+    }
+  }
+
+  downTemp = () => {
+    if(this.temperature > 10){
+      this.temperature -= 1
+    }
+  }
+
+  PowerSaving = (state) => {
+    if(state === true){
+      this.maxtemp = 25
+    }
+    else if(state === false){
+      this.maxtemp = 32
+    }
+  }
+
+  reset = () => {
+    this.temperature = 20
+  }
+
+  CurrentEnergyUsage = () => {
+    if(this.temperature < 18){
+      return "Low-usage"
+    } else if(this.temperature <= 25){
+      return "Medium-usage"
+    } else {
+      return "High-usage"
+    }
+  }
+
 }
 
 module.exports = Thermostat; 
--- thermostat/thermostat.test.js
+++ thermostat/thermostat.test.js
@@ -15,4 +15,62 @@ describe("Thermostat", () => {
     thermostat.upTemp();
     expect(thermostat.getTemperature()).toBe(21);
   });
+
+  it("Decreases the thermometer temp once to 19", () => {
+    thermostat.downTemp();
+    expect(thermostat.getTemperature()).toBe(19);
+  });
+
+  it("Increases the thermometer temp 6 times - max is 25", () => {
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    expect(thermostat.getTemperature()).toBe(25);
+  });
+
+  it("turns off powersaving mode", () => {
+    thermostat.PowerSaving(false);
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    expect(thermostat.getTemperature()).toBe(26);
+  });
+
+  it("resets the thermostat temp to 20", () => {
+    thermostat.upTemp();
+    expect(thermostat.getTemperature()).toBe(21);
+    thermostat.reset();
+    expect(thermostat.getTemperature()).toBe(20);
+  });
+
+  it('returns "low energy usage" when the temp is 17', () => {
+    thermostat.downTemp();
+    thermostat.downTemp();
+    thermostat.downTemp();
+    expect(thermostat.CurrentEnergyUsage()).toBe("Low-usage");
+  });
+
+  it('returns "medium energy usage" when the temp is 20', () => {
+    expect(thermostat.CurrentEnergyUsage()).toBe("Medium-usage");
+  });
+
+  it('returns "high energy usage" when the temp is 26', () => {
+    thermostat.PowerSaving(false);
+
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+    thermostat.upTemp();
+
+    expect(thermostat.CurrentEnergyUsage()).toBe("High-usage");
+  });
+
 });
